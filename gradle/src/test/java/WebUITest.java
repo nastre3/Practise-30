19e353ce02b9dc5d1aa102eeb048cb73168cf950
Anastasia <nastre70@gmail.com>
2022-01-25 20:16:55 +1000

    Added ApiTest, CalcTestm WebUITest

---
+++ gradle/src/test/java/WebUITest.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class WebUITest {
+
+    private WebDriver webDriver;
+
+    @Before
+    public void setup() {
+        // WebDriverManager.chromedriver().setup();
+        // webDriver = new ChromeDriver();
+    }
+
+    @Test
+    public void webTest() {
+        // webDriver.navigate().to("https://www.google.com/");
+    }
+
+    @After
+    public void tearDown() {
+        // webDriver.close();
+    }
+}
